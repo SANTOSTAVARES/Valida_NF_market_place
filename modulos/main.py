c422d57ce76944a32bcae96b3713e73261ffb930
SANTOSTAVARES <gsantostavares@gmail.com>
2022-02-28 14:00:20 -0300

    Inserí um comentário para teste

--- modulos/main.py
+++ modulos/main.py
@@ -6,5 +6,7 @@ referências
         https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
 '''
 
+#O objetivo do programa é validar se as notas fiscais das vendas realizadas num market place que não são feitas pelo proprietário do site (e sim por um terceiro), estão tendo notas fiscais emitidas com seus componentes corretos , por exemplo, descrição do produto, quantidade de itens, preço e emissor da nota e etc.
+
 if __name__ == "__main__":
     main()

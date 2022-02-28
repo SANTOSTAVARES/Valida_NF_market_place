c2d7f5407b628c9bbc5d36927dd82cea8308454a
SANTOSTAVARES <gsantostavares@gmail.com>
2022-02-28 13:19:33 -0300

    Meu primeiro commit

---
+++ modulos/main.py
@@ -0,0 +1,10 @@
+import untangle
+import xmltodict
+'''
+referências
+    Como interpretar aquivo xml
+        https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
+'''
+
+if __name__ == "__main__":
+    main()

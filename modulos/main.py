6e3c527b9f6f1ccc9b987e9e678201ca265de724
SANTOSTAVARES <gsantostavares@gmail.com>
2022-03-02 15:58:43 -0300

    arquivo xml

--- modulos/main.py
+++ modulos/main.py
@@ -1,17 +1,25 @@
 from xml.dom import minidom
 import xml.etree.ElementTree as ET
 
-class NFe_parceiro:
+class third_party_invoice:
 
-    def __init__(self, nome_arquivo) -> None:
-        self.nome_arquivo = nome_arquivo
+    def __init__(self, file_name) -> None:
+        self.file_name = file_name
+        invoice_file = open(self.file_name)
+        global invoice_content
+        global invoice_tag
+        invoice_tag = invoice_content.getElementsByTagName(f'{xml_tag}')
+        invoice_content = minidom.parse(invoice_file)
+        
+    def invoice_number(self):
+        xml_tag = 'nNF'
+        invoice_tag = invoice_content.getElementsByTagName(f'{xml_tag}')
+        return invoice_tag[0].firstChild.data
     
-    arquivo = open(f'{self.nome_arquivo}')
-    conteudo_NF = minidom.parse(arquivo)
-
     def cnpj(self):
-        return conteudo_NF.getElementsByTagName('CNPJ')
+        xml_tag = 'cnpj'
+        invoice_tag = invoice_content.getElementsByTagName(f'{xml_tag}')
+        return invoice_tag[0].firstChild.data
 
-nerd = NFe_parceiro('nota_nerdstore.xml')
-
-nerd.cnpj()
+nerd = third_party_invoice('nota_nerdstore.xml')
+print(nerd.invoice_number())

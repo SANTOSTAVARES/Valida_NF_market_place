5ff1ac224181ee3f091d7cfadfc08ce2494b961c
Gabriel Tavares <58869758+SANTOSTAVARES@users.noreply.github.com>
2022-03-04 22:18:19 -0300

    Update main.py

--- modulos/main.py
+++ modulos/main.py
@@ -1,6 +1,7 @@
 from xml.dom import minidom
 import xml.etree.ElementTree as ET
 from data import not_checked_invoices
+import time
 
 class third_party_invoice:
 
@@ -36,18 +37,22 @@ class third_party_invoice:
         
         counter =  0
         for x in unitary_value:
-            products_list[counter][1] = x.firstChild.data
+            products_list[counter][1] = x.firstChild.data[:-8]
             counter += 1
 
         return products_list
 
+def search_invoice():
 
-#print(nerd.invoice_number())
-#print(nerd.cnpj())
-#print(nerd.products())
-
-counter = 0
-first_in_line = not_checked_invoices()[counter][0]
-print(first_in_line)
-analyze_invoice = third_party_invoice(f'{first_in_line}.xml')
-print(analyze_invoice.products())
+    while True:    
+        counter = 0
+        if len(not_checked_invoices()) > 0:
+            for i in range(len(not_checked_invoices())):
+                first_in_line = not_checked_invoices()[counter][0]
+                analyze_invoice = third_party_invoice(f'{first_in_line}.xml')
+                print(analyze_invoice.products())
+                counter += 1
+        else:
+            print("There isn't any invoice to check.")
+            time.sleep(20)
+            print("It's going to be checked, if there is invoice in line.")

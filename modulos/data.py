0503716421de081372326654886081a1ade08904
SANTOSTAVARES <gsantostavares@gmail.com>
2022-03-08 21:08:50 -0300

    Versão final

--- modulos/data.py
+++ modulos/data.py
@@ -1,3 +1,4 @@
+from multiprocessing import connection
 import sqlite3
 from contextlib import closing
 
@@ -16,9 +17,7 @@ def invoice_check():
 invoices_examples = [
 ['NFe31200806347409006953550190002601621173891452', 'newbie'],
 ['NFe31200806347409006953550310003371541174045999', 'newbie'],
-['NFe33200733240768000191550010000029001492586314', 'newbie'],
-['NFe35191142274696002561550010040410901297528776', 'newbie'],
-['NFe41210810640539000401550010000017581310588369', 'newbie']
+['NFe35191142274696002561550010040410901297528776', 'newbie']
 ]
 
 def insert_examples():
@@ -41,8 +40,9 @@ def all_invoices():
         print(i)
     connection.close()
 
-to_be_checked = []
+
 def not_checked_invoices():
+    to_be_checked = []
     with sqlite3.connect("invoice_check.db") as connection:
         with closing(connection.cursor()) as cursor:
             cursor.execute("select * from invoice_check where checking = 'newbie'")
@@ -53,3 +53,63 @@ def not_checked_invoices():
                 iterable_result = [query_result[1], query_result[2]]
                 to_be_checked.append(iterable_result)
     return to_be_checked
+
+def sales_table():
+    connection = sqlite3.connect("sold_products.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE sold_products (
+      id integer primary key autoincrement,
+      product text,
+      price text,
+      id_NFe
+    );
+    """)
+    connection.close()
+
+sold_products_examples = [
+    ['MUNHEQUEIRA TENNIS WB L', '17.99', 'NFe35191142274696002561550010040410901297528776'],
+    ['CAMISETA ID LINEAGE TEE', '47.99', 'NFe35191142274696002561550010040410901297528776'],
+    ['SHORTS 3S KNIT W', '42.00', 'NFe35191142274696002561550010040410901297528776'],
+    ['CAMISETA BB PRINT TEE 1', '59.99', 'NFe35191142274696002561550010040410901297528776'], 
+    ['CAMISETA M GRFX LNR T 2', '42.00', 'NFe35191142274696002561550010040410901297528776'],
+    ['CAMISETA M V M', '47.99', 'NFe35191142274696002561550010040410901297528776'],
+    ['BERMU MP OXER PERF TRAINING 5G 204', '21.24', 'NFe31200806347409006953550190002601621173891452'], 
+    ['BERMU MP OXER PERF TRAINING 62 204', '21.24', 'NFe31200806347409006953550190002601621173891452'],
+    ['FUNCIONA OXER PERF 32 MOLA AC 02 000', '299.99', 'NFe31200806347409006953550310003371541174045999'], 
+    ['FUNCIONA ACTE PERF T12 3CM 02 221', '89.99', 'NFe31200806347409006953550310003371541174045999']
+]
+
+def insert_sales_examples():
+    connection = sqlite3.connect("sold_products.db")
+    cursor = connection.cursor()
+    cursor.executemany("""
+    INSERT INTO sold_products (product, price, id_NFe) 
+    VALUES (?,?,?)
+    """, sold_products_examples)
+    connection.commit()
+    connection.close()
+
+def delete_record():
+    
+    with sqlite3.connect("sold_products.db") as connection:
+        with closing(connection.cursor()) as cursor:
+            cursor.execute("""delete from sold_products
+                where product = "BERMU MP OXER PERF TRAINING 62 204" """)
+            print("Registros apagados: ", cursor.rowcount)
+            if cursor.rowcount == 1:
+                connection.commit()
+                print("Alterações gravadas.")
+            else:
+                connection.rollback()
+                print("Alterações abortadas")
+
+def all_sales_examples():
+    connection = sqlite3.connect("sold_products.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT * FROM sold_products;
+    """)
+    for i in cursor.fetchall():
+        print(i)
+    connection.close()
--- modulos/main.py
+++ modulos/main.py
@@ -1,26 +1,32 @@
 from xml.dom import minidom
 import xml.etree.ElementTree as ET
 from data import not_checked_invoices
-import time
-#Aulas com Dirceu
+import sqlite3
+from contextlib import closing
+
 class third_party_invoice:
+    """Get data from xml file to access information by this object.
+    The parameter must be the file without .xml"""
 
     def __init__(self, file_name) -> None:
         self.file_name = file_name
-        invoice_file = open(self.file_name)
+        invoice_file = open(f"../NFe/{self.file_name}")
         global invoice_content
         invoice_content = minidom.parse(invoice_file)
-                
+
+    #This fuction returns the invoice number.            
     def invoice_number(self):
         xml_tag = 'nNF'
         invoice_tag = invoice_content.getElementsByTagName(f'{xml_tag}')
         return invoice_tag[0].firstChild.data
     
+    #This function returns the sales company CNPJ.
     def cnpj(self):
         xml_tag = 'CNPJ'
         invoice_tag = invoice_content.getElementsByTagName(f'{xml_tag}')
         return invoice_tag[0].firstChild.data
 
+    #This function  returns the product name and prince in Python list.
     def products(self):
         products_list = []
         items_list = invoice_content.getElementsByTagName('det')
@@ -43,16 +49,36 @@ class third_party_invoice:
         return products_list
 
 def search_invoice():
+    """Search in database, if there is invoice to be analyzed.
+    This function reads only the first invoice has to be analyzed. It was not implemented loop in this function.
+    In the end of this function, It prints the analysis."""
 
-    while True:    
-        counter = 0
-        if len(not_checked_invoices()) > 0:
-            for i in range(len(not_checked_invoices())):
-                first_in_line = not_checked_invoices()[counter][0]
-                analyze_invoice = third_party_invoice(f'{first_in_line}.xml')
-                print(analyze_invoice.products())
-                counter += 1
-        else:
-            print("There isn't any invoice to check.")
-            time.sleep(20)
-            print("It's going to be checked, if there is invoice in line.")
+    
+    if len(not_checked_invoices()) > 0: #Check if there is invoice in line in database to be checked.
+        first_in_line = not_checked_invoices()[0][0]
+        analyze_invoice = third_party_invoice(f'{first_in_line}.xml')
+        main_report = analyze_invoice.products()
+                                    
+        to_be_checked = []
+        with sqlite3.connect("sold_products.db") as connection:
+            with closing(connection.cursor()) as cursor:
+                cursor.execute("select * from sold_products where id_NFe=:chave", {"chave": first_in_line})
+                while True:
+                    query_result = cursor.fetchone()
+                    if query_result is None:
+                        break
+                    iterable_result = [query_result[1], query_result[2]]
+                    to_be_checked.append(iterable_result) 
+        
+        for i in main_report:
+            i.append('')
+        for x in main_report:
+            for y in to_be_checked:
+                if x[0] == y[0]:
+                    x[2] = 'REGULAR'
+        for i in main_report:
+            if i[2] == '':
+                i[2] = 'IRREGULAR'
+        print(main_report)            
+    else:
+        print("There isn't any invoice to check yet.")
